7d8543b79eab45c019cff4c4f6cfccbcb1b1beea
StephenHwang <sjhwang@ucsc.edu>
2022-04-15 12:08:12 -0700

    Add comment on setting pulseadio bluetooth headphone switch.

--- bin/bluetooth_toggle.sh
+++ bin/bluetooth_toggle.sh
@@ -4,3 +4,6 @@ hcitool dev | grep hci > /dev/null &&
   rfkill block bluetooth ||
     rfkill unblock bluetooth
 
+# for disable autoswitch bluetooth profiles: (also ensure  talk-to-speak is disabled)
+#     /etc/pulse/default.pa
+#     load-module module-bluetooth-policy auto_switch=0
