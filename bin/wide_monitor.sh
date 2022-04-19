c2b014355665a2f494cc540cb538dc5ad5ea004f
StephenHwang <sjhwang@ucsc.edu>
2022-04-18 20:54:41 -0700

    Disable touchpad on wide monitor.

--- bin/wide_monitor.sh
+++ bin/wide_monitor.sh
@@ -5,3 +5,5 @@ feh --bg-fill /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg /home/stephen
 killall conky
 sleep 0.5
 conky
+
+eval "xinput disable $(xinput list | grep "Synaptics" | sed 's/.*id=//' | cut -f1)"
