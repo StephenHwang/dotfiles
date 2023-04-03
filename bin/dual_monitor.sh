fe80f0ac304c347907196db1c3e68c6402c87764
StephenHwang <sjhwang@ucsc.edu>
2023-04-03 02:39:22 -0400

    Ajust scripts.

--- bin/dual_monitor.sh
+++ bin/dual_monitor.sh
@@ -3,11 +3,14 @@
 # feh --bg-fill /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg /home/stephen/Pictures/wallpapers/vertical_mtn.jpg /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg
 
 # updated naming, with 3 monitors
-# feh --bg-fill /home/stephen/Pictures/wallpapers/vertical_mtn.jpg /home/stephen/Pictures/wallpapers/vertical_mtn.jpg /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg 
+# feh --bg-fill /home/stephen/Pictures/wallpapers/vertical_mtn.jpg /home/stephen/Pictures/wallpapers/vertical_mtn.jpg /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg
+
+touchpad_device_id=$(xinput list | grep "Synaptics" | sed 's/.*id=//' | cut -f1)
+eval "xinput enable $touchpad_device_id"
 
 # docked dual
 autorandr --load docked
-feh --bg-fill /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg /home/stephen/Pictures/wallpapers/vertical_mtn.jpg 
+feh --bg-fill /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg /home/stephen/Pictures/wallpapers/vertical_mtn.jpg
 
 killall conky
 sleep 0.5
---
+++ bin/laptop_left_of_external.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# docked dual
+autorandr --load laptop_left_of_monitor
+# feh --bg-fill /home/stephen/Pictures/wallpapers/horizontal_mtn.jpg /home/stephen/Pictures/wallpapers/vertical_mtn.jpg
+
+# killall conky
+# sleep 0.5
+# conky
